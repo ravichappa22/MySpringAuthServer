89baccb4d160a46cfd735ffbbd4b7a723de9c8d5
ravchappa22 <chapparavi22@gmail.com>
2017-08-27 11:04:40 -0400

    added jwt token

--- src/main/java/com/my/company/auths/AuthorizationServerConfig.java
+++ src/main/java/com/my/company/auths/AuthorizationServerConfig.java
@@ -3,24 +3,34 @@ package com.my.company.auths;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.TokenStore;
-import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
+import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
+import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.security.oauth2.provider.token.store.KeyStoreKeyFactory;
 
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter{
+	
 	@Autowired
     private AuthenticationManager authenticationManager;
+	
+	@Autowired
+	private Environment environment;
 	
 	@Override
     public void configure(final AuthorizationServerEndpointsConfigurer endpoints) {
 		endpoints.tokenStore(tokenStore())
-			.authenticationManager(authenticationManager);
+			.tokenEnhancer(jwtTokenEnhancer())
+				.authenticationManager(authenticationManager);
     }
 	
 	@Override	
@@ -29,15 +39,36 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		        .withClient("usemyname") 
 		        .secret("mypassword")
 		        .scopes("read", "write") 
-		        .authorities("ROLE_USER")  
+		        .authorities("ROLE_RS_READ")  
 		        .authorizedGrantTypes("client_credentials")
-		        .accessTokenValiditySeconds(10);
+		        .accessTokenValiditySeconds(600);
 	 }
 	
 	
 	@Bean
 	public TokenStore tokenStore() {
-		return new InMemoryTokenStore();
+		//return new InMemoryTokenStore();
+		return new JwtTokenStore(jwtTokenEnhancer());
 	}
+	
+	@Bean
+	protected JwtAccessTokenConverter jwtTokenEnhancer() {
+		String pwd = environment.getProperty("keystore.password");
+		KeyStoreKeyFactory keyStoreKeyFactory = new KeyStoreKeyFactory(new ClassPathResource("jwt.jks"),
+				pwd.toCharArray());
+		JwtAccessTokenConverter converter = new JwtAccessTokenConverter();
+		converter.setKeyPair(keyStoreKeyFactory.getKeyPair("jwt"));
+		return converter;
+	}
+
+	@Override
+	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
+		security.tokenKeyAccess("permitAll()")
+	    		.checkTokenAccess("isAuthenticated()");
+		
+		
+	}
+	
+	
 
 }
--- src/main/java/com/my/company/auths/SecurityConfiguration.java
+++ src/main/java/com/my/company/auths/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.my.company.auths;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -10,9 +11,18 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter{
 	@Override
 	public void configure(WebSecurity web) throws Exception {
 		super.configure(web);
-		//web.ignoring().antMatchers("oauth/token","/oauth/check_token");
+		web.ignoring().antMatchers("oauth/token","/oauth/check_token","oauth/token_key");
 		
 	}
+
+	/*@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		super.configure(http);
+		http.authorizeRequests().antMatchers("oauth/token","/oauth/check_token","oauth/token_key")
+						.permitAll();
+	}*/
+	
+	
 
 	
 }
